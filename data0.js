bf5eb375231dcc1351308b3da04c38d5806cd604
Mikesael Nieves <97325479+LlamaTrunks@users.noreply.github.com>
2022-02-08 19:12:27 -0500

    Update data0.js

--- data0.js
+++ data0.js
@@ -19,3 +19,13 @@ function stop() {
     
 }
 
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.play = function() {
+        this.sound.play();
+    }
+    // this.stop = function() {
+    //     this.sound.pause();
+    // }
+}
